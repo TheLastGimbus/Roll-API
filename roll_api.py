24ac591c09380b8eafcb95fd370a7bf2173ae74e
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-04-05 00:33:49 +0200

    Very initial structure

--- roll_api.py
+++ roll_api.py
@@ -1,8 +1,52 @@
+import gpiozero
+import pathlib
+import subprocess
+import uuid
 from flask import Flask
+from flask import send_file
+from time import sleep
 
 app = Flask(__name__)
 
+API1 = '/api/v1'
 
-@app.route('/api/v1')
+FULL_IMAGES = pathlib.Path('full_images/')
+
+led = gpiozero.LED(4)
+
+
+def roll_dice():
+    s = gpiozero.Servo(14, min_pulse_width=1 / 2000)  # Default pulse isn't getting full 180 degrees
+    s.min()
+    sleep(0.5)
+    s.value = 0.9
+    sleep(0.5)
+    s.close()  # Close it so it doesn't rattle in there
+
+
+# https://raspberrypi.stackexchange.com/questions/51406/cannot-connect-to-picamera-when-using-it-with-flask
+# Change this later possibly
+def take_picture(out_name):
+    led.on()
+    subprocess.run(f'raspistill -o {out_name}'.split(' '))
+    led.off()
+
+
+@app.route(API1)
 def hello():
-    return {"message": "Hello there!"}
+    return "Hello there!"
+
+
+@app.route(API1 + '/roll')
+def roll():
+    roll_dice()
+    image_id = str(uuid.uuid4())
+    pic_name = FULL_IMAGES / f'{image_id}.jpg'
+    take_picture(pic_name)
+    # TODO: Recognize dice
+    return {'full_image_id': image_id}
+
+
+@app.route(API1 + '/image/<uuid:image_id>')
+def image(image_id):
+    return send_file(FULL_IMAGES / f'{image_id}.jpg')
