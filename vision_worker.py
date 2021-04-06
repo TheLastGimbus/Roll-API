79c3d72625928ddb0b9bbb12be892d144eae1403
TheLastGimbus <mateusz.soszynski@tuta.io>
2021-04-06 19:11:43 +0200

    Separate worker becaus cv2 takes ages to load

---
+++ vision_worker.py
@@ -0,0 +1,9 @@
+import cv2  # This takes SUPER long on my Pi Zero 0_o
+import numpy as np
+import rq
+import sys
+
+with rq.Connection():
+    qs = sys.argv[1:] or ['vision']
+    w = rq.Worker(qs)
+    w.work()
